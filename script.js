7854c016e8055257c4b5e22f621ec6bf95ae6412
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2019-02-19 19:21:48 +0530

    adding footer and contact page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Side Menu Navigator
 function openNav() {
   if(document.getElementById('side-nav').style.display === "flex"){
@@ -11,10 +13,12 @@ function openNav() {
 // Change Navigation color on scroll
 function Scroll(){
   var scrollPage = window.pageYOffset;
-  if(scrollPage > 100){
+  if(scrollPage > 90){
     document.getElementById('nav-container').style.backgroundColor = "black";
+    // document.getElementById('nav-container').style.height = "70px";
   }else{
     document.getElementById('nav-container').style.backgroundColor = "";
+    // document.getElementById('nav-container').style.height = "";
   }
 
 }
@@ -26,3 +30,48 @@ window.addEventListener("scroll", Scroll);
 const date = new Date().getUTCFullYear();
 document.getElementById('date').innerHTML = date;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var token = '1362124742.7b33a8d.6613a3567f0a425f9852055b8ef743b7',
+//     num_photos = 10,
+//     container = document.getElementById( 'rudr_instafeed' ),
+//     scrElement = document.createElement( 'script' );
+ 
+// window.mishaProcessResult = function( data ) {
+// 	for( x in data.data ){
+// 		container.innerHTML += '<li><img src="' + data.data[x].images.low_resolution.url + '"></li>';
+// 	}
+// }
+ 
+// scrElement.setAttribute( 'src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult' );
+// document.body.appendChild( scrElement );
+
+
+
+
+
+// <!-- Script to get the input value from form -->
+
+// <!-- <script>
+//   document.getElementById('submit').addEventListener('click',(e) => {
+//     // alert('hello');
+//     var name = document.getElementById('name').value,
+//         email = document.getElementById('email').value,
+//         tel = document.getElementById('tel').value,
+//         textarea = document.getElementById('textarea').value;
+
+//         console.log(`name entered is ${name}, email is ${email}, telephone is ${tel} and message is ${textarea}`);
+//         e.preventDefault();
+//   })
+// </script> -->

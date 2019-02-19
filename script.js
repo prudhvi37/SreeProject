bb7e46b00e9f9a57fb0d2e040b2aba93e1c52f38
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2019-02-19 23:05:11 +0530

    changing necessary changes in schedule input fields

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Side Menu Navigator
 function openNav() {
   if(document.getElementById('side-nav').style.display === "flex"){
@@ -9,10 +7,6 @@ function openNav() {
     document.getElementById('side-nav').style.flexDirection = "column";
   }
 }
-
-
-
-
 
 // Change Navigation color on scroll
 function Scroll(){
@@ -41,41 +35,3 @@ document.getElementById('date').innerHTML = date;
 
 
 
-
-
-
-
-
-
-// var token = '1362124742.7b33a8d.6613a3567f0a425f9852055b8ef743b7',
-//     num_photos = 10,
-//     container = document.getElementById( 'rudr_instafeed' ),
-//     scrElement = document.createElement( 'script' );
- 
-// window.mishaProcessResult = function( data ) {
-// 	for( x in data.data ){
-// 		container.innerHTML += '<li><img src="' + data.data[x].images.low_resolution.url + '"></li>';
-// 	}
-// }
- 
-// scrElement.setAttribute( 'src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult' );
-// document.body.appendChild( scrElement );
-
-
-
-
-
-// <!-- Script to get the input value from form -->
-
-// <!-- <script>
-//   document.getElementById('submit').addEventListener('click',(e) => {
-//     // alert('hello');
-//     var name = document.getElementById('name').value,
-//         email = document.getElementById('email').value,
-//         tel = document.getElementById('tel').value,
-//         textarea = document.getElementById('textarea').value;
-
-//         console.log(`name entered is ${name}, email is ${email}, telephone is ${tel} and message is ${textarea}`);
-//         e.preventDefault();
-//   })
-// </script> -->

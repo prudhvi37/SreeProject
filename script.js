fd57f3c9c83f7b4d3d72a1245057ed1c0046bdbc
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2019-02-21 14:32:59 +0530

    adding testimonals

--- script.js
+++ script.js
@@ -35,3 +35,25 @@ document.getElementById('date').innerHTML = date;
 
 
 
+// instagram posts
+
+
+
+
+
+var token = '11146853612.1677ed0.4829171de9604ddfa20234819a826856',
+    num_photos = 0,
+    container = document.getElementById( 'instagram' ),
+    scrElement = document.createElement( 'script' );
+ 
+window.mishaProcessResult = function( dataFromInsta ) {
+	for( x in dataFromInsta.data ){
+		container.innerHTML += '<li><img src="' + dataFromInsta.data[x].images.low_resolution.url + '"></li>';
+  }
+  // dataFromInsta.forEach(function(value,x,arr){
+  //     container.innerHTML += '<li><img src="' + value.data[arr].images.low_resolution.url + '"></li>';
+  // });
+}
+ 
+scrElement.setAttribute( 'src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult' );
+document.body.appendChild( scrElement );
